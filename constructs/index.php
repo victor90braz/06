0cb98ab5a17d703ab60286c11f51efc6ef5d62f7
victor braz <victor.90braz@gmail.com>
2023-10-11 17:22:58 +0200

    ReflectionMethod access and private function

--- constructs/index.php
+++ constructs/index.php
@@ -2,51 +2,22 @@
 $basePath = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\object-oriented-principles-php\\06\\";
 require_once $basePath . "functions.php";
 
-interface NewsLetter {
-    public function subscribe($email);
-}
+class Person {
 
-class CampaignMonitor implements NewsLetter {
+    public $name;
 
-    /**
-     * Subscribe the user using CampaignMonitor.
-     *
-     * @param string $email The email address to subscribe.
-     */
-    public function subscribe($email) {
-        dd('subscribe with CampaignMonitor');
+    public function __construct($name) {
+        $this->$name;
+    }
+
+    private function password() {
+        return 'hello world';
     }
 }
 
-class Drip implements NewsLetter {
+$method = new \ReflectionMethod(Person::class, 'password');
+$method->setAccessible(true);
 
-    /**
-     * Subscribe the user using Drip.
-     *
-     * @param string $email The email address to subscribe.
-     */
-    public function subscribe($email) {
-        dd('subscribe with drip');
-    }
+$person = new Person('Bob');
 
-}
-
-class NewsLetterSubscriptionsController {
-
-    /**
-     * Store the subscription using the provided class reference.
-     *
-     * @param object $newsLetter An instance of a subscription service class.
-     */
-    public function store(NewsLetter $newsLetter) {
-
-        $email = 'victor@gmail.com';
-
-        $newsLetter->subscribe($email);
-    }
-}
-
-$controller = new NewsLetterSubscriptionsController();
-
-$controller->store(new CampaignMonitor());
-
+dd($method->invoke($person));
